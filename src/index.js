23f58106fc22868edeffddadafe65457c394e84c
TousifTamboli <tousiftamboli3@gmail.com>
2026-04-10 00:11:47 +0530

    ready to deploy for initial phase

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
   }),
 );
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
